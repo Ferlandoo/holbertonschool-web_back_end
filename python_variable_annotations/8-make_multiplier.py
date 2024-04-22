a7e49be962a4532d7e02043364b9d42a5d6bce56
Ferlando <contact@ferlando.me>
2024-04-22 11:51:36 +0200

    add ex 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 """Type-annotated function make_multiplier"""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> 
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by multiplier"""
+    def multiply(n: float) -> float:
+        """Multiply a float by multiplier"""
+        return n * multiplier
+    return multiply
+
