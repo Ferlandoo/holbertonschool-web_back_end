2be3b2c1f3c9594d2b25ae29cbe38120df14731e
Ferlando <contact@ferlando.me>
2024-04-22 13:20:31 +0200

    update typing

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Type-annotated function element_length"""
-from typing import Sequence, Tuple, List
+"""Function parameters and return values with the appropriate types"""
+from typing import Sequence, Tuple, List, Iterable
 
 
-def element_length(lst: Sequence[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Returns a list of tuples"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
