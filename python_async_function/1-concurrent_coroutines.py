375890400f31672b83a816b35131910b5595b818
Ferlando <contact@ferlando.me>
2024-04-22 14:52:01 +0200

    update

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """Basic async syntax"""
-import random
+from typing import List
 import asyncio
+import random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for a random delay and return it"""
     elem_list = []
     for _ in range(n):
         delay = random.uniform(0, max_delay)
         await asyncio.sleep(delay)
         elem_list.append(delay)
-    return elem_list
+    return sorted(elem_list)
