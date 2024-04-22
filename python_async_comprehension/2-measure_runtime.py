94214292584f4c075dc7be136c07c6384cac8bdf
Ferlando <contact@ferlando.me>
2024-04-22 17:05:54 +0200

    add ex 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the runtime of async_comprehension"""
+    start = time.time()
+    await async_comprehension()
+    end = time.time()
+    return end - start
