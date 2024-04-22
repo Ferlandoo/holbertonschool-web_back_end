49944847c16e6c0a257f303ceada5c62b6310cc1
Ferlando <contact@ferlando.me>
2024-04-22 16:58:34 +0200

    update return

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async Generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Loop 10 times with wait 1 sec"""
     for _ in range(10):
         await asyncio.sleep(1)
