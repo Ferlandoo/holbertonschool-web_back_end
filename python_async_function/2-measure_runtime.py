33bdc5001da12e18d62be3ce3184e6b8d56c4eb0
Ferlando <contact@ferlando.me>
2024-04-22 15:57:36 +0200

    update

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """Basic async syntax"""
-from typing import List
 import asyncio
 import random
 
 
-async def measure_time(n: int, max_delay: int) -> List[float]:
+async def measure_time(n: int, max_delay: int) -> float:
     """Wait for a random delay and return it"""
-    elem_list = []
     start = asyncio.get_event_loop().time()
+    elem_list = []
     for _ in range(n):
         delay = random.uniform(0, max_delay)
         await asyncio.sleep(delay)
