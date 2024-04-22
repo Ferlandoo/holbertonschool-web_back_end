024034263c33fd16ec2c59afc421e18e069f4f4f
Ferlando <contact@ferlando.me>
2024-04-22 11:43:44 +0200

    update ex 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Type-annotated function to_kv"""
+from typing import Union
 
 
-def to_kv(k: str, v: int | float) -> tuple:
-    return k, v
+def to_kv(k: str, v: Union[int | float]) -> Union[str, float]:
+    """Returns a tuple with a string and a float"""
+    return (k, v * v)
