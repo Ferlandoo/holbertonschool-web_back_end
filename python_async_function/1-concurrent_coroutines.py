979301c960397659667cbe6d5d1e1912bb03eee5
Ferlando <contact@ferlando.me>
2024-04-22 14:48:53 +0200

    add ex 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic async syntax"""
+import random
+import asyncio
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Wait for a random delay and return it"""
+    elem_list = []
+    for _ in range(n):
+        delay = random.uniform(0, max_delay)
+        await asyncio.sleep(delay)
+        elem_list.append(delay)
+    return elem_list
