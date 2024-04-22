c0f5081d4fc491eefad676de95c2d080f898fc78
Ferlando <contact@ferlando.me>
2024-04-22 16:00:54 +0200

    add time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,16 +2,17 @@
 """Basic async syntax"""
 import asyncio
 import random
+import time
 
 
 async def measure_time(n: int, max_delay: int) -> float:
     """Wait for a random delay and return it"""
-    start = asyncio.get_event_loop().time()
+    start = time.time()
     elem_list = []
     for _ in range(n):
         delay = random.uniform(0, max_delay)
         await asyncio.sleep(delay)
         elem_list.append(delay)
-    end = asyncio.get_event_loop().time()
+    end = time.time()
     total_time = end - start
     return total_time / n
