857a83be3a1fc535833fe2a4648d3c5d0ea88655
Ferlando <contact@ferlando.me>
2024-04-22 10:50:24 +0200

    add ex 2

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Type-annotated function add"""
+"""Type-annotated function concat"""
 
 
 def concat(str1: str, str2: str) -> str:
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Type-annotated function floor"""
+
+
+def floor(n: float) -> float:
+    return int(n)
