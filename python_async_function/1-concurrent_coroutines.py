da24329ab9031cb30f711045b552eeccd33fc656
Ferlando <contact@ferlando.me>
2024-04-22 15:16:11 +0200

    update list

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """Basic async syntax"""
-from typing import List
 import asyncio
 import random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """Wait for a random delay and return it"""
     elem_list = []
     for _ in range(n):
