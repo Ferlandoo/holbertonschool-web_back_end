69adbb0afd45474bbd0ca686f98bd5040793daab
Ferlando <contact@ferlando.me>
2024-04-23 10:05:15 +0200

    update

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,6 +10,6 @@ async def measure_runtime() -> float:
     """Measure the runtime of async_comprehension called
     four times in parallel."""
     start = time.time()
-    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end = time.time()
     return end - start
