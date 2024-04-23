1641ca27d5157488af393fb35585753e11746c9b
Ferlando <contact@ferlando.me>
2024-04-23 09:57:11 +0200

    add gather

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """Tasks"""
 from typing import List
+import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Return a list of delays"""
-    elem_list = []
-    for _ in range(n):
-        elem_list.append(await task_wait_random(max_delay))
-    return sorted(elem_list)
+async def measure_runtime() -> float:
+    """Measures the runtime"""
+    import time
+    start = time.time()
+    tasks = [task_wait_random() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    end = time.time()
+    return end - start
