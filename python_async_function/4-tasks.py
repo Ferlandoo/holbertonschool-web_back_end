f59b56745aa0db0028b620e1b18ab6f36a3210c8
Ferlando <contact@ferlando.me>
2024-04-23 09:58:43 +0200

    restore 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 """Tasks"""
 from typing import List
-import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def measure_runtime() -> float:
-    """Measures the runtime"""
-    import time
-    start = time.time()
-    tasks = [task_wait_random() for _ in range(4)]
-    await asyncio.gather(*tasks)
-    end = time.time()
-    return end - start
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Return a list of delays"""
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in delays]
