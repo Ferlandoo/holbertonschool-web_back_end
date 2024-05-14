c4a9e945488933b4507064b56b2cce66ee4dc579
Ferlando <contact@ferlando.me>
2024-05-14 14:05:33 +0200

    add ex 8

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+export default function divideFunction(numerator, denominator) {
+  return new Promise((resolve, reject) => {
+    if (denominator === 0) {
+      reject(new Error('ZeroDivisionError'));
+    } else {
+      resolve(numerator / denominator);
+    }
+  });
+}
