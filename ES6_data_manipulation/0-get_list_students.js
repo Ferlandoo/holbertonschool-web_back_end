36b3fa00c56f62850f49b3f0f2ac248af237fc0d
Ferlando <contact@ferlando.me>
2024-05-20 11:41:08 +0200

    update

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,18 @@
-/*eslint-disable*/
-export default getListStudents = () => [
-  { id: 1, name: 'Guillaume', location: 'San Francisco' },
-  { id: 2, name: 'James', location: 'Columbia' },
-  { id: 5, name: 'Serena', location: 'Francisco' },
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
 ];
+export default getListStudents;
