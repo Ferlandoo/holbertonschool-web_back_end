eb38019ddeb9d7cca3e2350c2456542a68d37550
Ferlando <contact@ferlando.me>
2024-05-20 11:05:44 +0200

    add ex 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const firstStudent = {
+    id: 1,
+    name: "Guillaume",
+    location: "San Francisco"
+  }
+
+  const secondStudent = {
+    id: 2,
+    name: "James",
+    location: "Columbia"
+  }
+
+  const thirdStudent = {
+    id: 5,
+    name: "Serena",
+    location: "Francisco"
+  }
+
+  return typeof [firstStudent, secondStudent, thirdStudent]
+}
