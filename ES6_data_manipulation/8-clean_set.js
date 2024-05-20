985ba3a29e796da1064c34d299b1b403c2ed9c42
Ferlando <contact@ferlando.me>
2024-05-20 20:21:46 +0200

    add ex 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,8 @@
 export default function cleanSet(argSet, startString) {
-
+  if (startString.length === 0) {
+    return '';
+  }
+  const cleanPart = new Set([...argSet].filter((text) => text.startsWith(startString)));
+  const newStr = Array.from(cleanPart).join('-');
+  return newStr.replace(new RegExp(startString, 'g'), '');
 }
